aa94a6877c04851bc66f933a9de76adf31f813ab
Rishabh <rishabh@supertokens.io>
2021-07-06 22:31:08 +0530

    changes for new repo

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Logout from "./Logout";
 import SuccessView from "./SuccessView";
 import { useSessionContext } from 'supertokens-auth-react/recipe/session';
-import { useHistory } from "react-router-dom";
 import { signOut } from "supertokens-auth-react/recipe/emailpassword";
 
 export default function Home() {
     const { userId } = useSessionContext();
-    const history = useHistory();
 
     async function logoutClicked() {
         await signOut();
-        history.push("/auth");
     }
 
     return (
